355730e48f29dfe8c051308c9166c8f666d1a7f6
Zbynek Rybicka <zbynek.rybicka@gmail.com>
2025-09-29 21:36:47 +0200

    fix: production script

--- scripts/prod.ps1
+++ scripts/prod.ps1
@@ -1,4 +1,6 @@
 param([string]$Email="test@demo.cz",[string]$Pass="heslo123")
+param([Parameter(Mandatory=$true)][string]$JwtPass)
 docker compose up -d --build
+docker compose exec php sh -lc "mkdir -p config/jwt; if [ ! -f config/jwt/private.pem ]; then openssl genrsa -aes256 -out config/jwt/private.pem -passout pass:$JwtPass 4096 && openssl rsa -pubout -in config/jwt/private.pem -passin pass:$JwtPass -out config/jwt/public.pem; fi"
 docker compose exec php php bin/console doctrine:migrations:migrate -n
 docker compose exec php php bin/console app:create-user $Email $Pass
